7e2ee69e00ee009ca9c74a2e365f711253b81c88
StevonS <110857291+StevonS@users.noreply.github.com>
2023-10-01 18:54:49 -0400

    Create HC12ArduinoReceiver.ino

---
+++ HC12ArduinoReceiver.ino
@@ -0,0 +1,44 @@
+
+
+// See SoftwareSerial example 
+// https://www.arduino.cc/en/tutorial/SoftwareSerialExample
+
+#include <SoftwareSerial.h>;
+#define RX 2 //Connect to the TX pin of the HC-12
+#define TX 3 //Connect to the RX pin of the HC-12
+SoftwareSerial HC12(RX, TX);
+bool armed = false;
+byte incomingByte;
+String readBuffer = "";
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600);
+
+}
+
+void loop() { // run over and over
+
+   while (HC12.available()) {             // If HC-12 has data
+    incomingByte = HC12.read();          // Store each icoming byte from HC-12
+    readBuffer += char(incomingByte);    // Add each byte to ReadBuffer string variable
+  }
+
+  if (readBuffer == "Armed") {
+    Serial.println("ARMED");
+    armed = true;
+  }
+
+  if (armed = true) {
+    HC12.write("On-Board armed")
+    if (HC12.available()) {
+      Serial.write(HC12.read());
+    }
+    if (Serial.available()) {
+      HC12.write(Serial.read());
+    }
+  }
+  
+
+
+}
