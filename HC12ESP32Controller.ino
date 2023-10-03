179c570147c412cf2b7e435e21188876ebc276a1
StevonS <110857291+StevonS@users.noreply.github.com>
2023-10-02 22:30:50 -0400

    Update HC12ESP32Controller.ino
    first working (no safety system)

--- HC12ESP32Controller.ino
+++ HC12ESP32Controller.ino
@@ -5,6 +5,7 @@
 bool armed = false;
 byte incomingByte;
 String readBuffer = "";
+bool startup = false;
 
 void setup() {
   Serial.begin(115200);
@@ -36,8 +37,13 @@ void loop() {
   while (Serial.available()) {      
     HC12.write(Serial.read());   
 
-
   }
+
+  if (readBuffer == "On-Board Armed"){
+    startup = true;
+    Serial.println("Starting Transmission:");
+  }
+  delay(200);
 }
 
 
@@ -54,11 +60,26 @@ void controller(){
       HC12.write("Disarmed");
     }
   }
-  if (PS4.R2())){
-    
+  //PS4.setLed(uint8_t r, uint8_t g, uint8_t b)
+  if (armed == true && startup == true){
+    HC12.print(PS4.R2Value());//sends the variables
+    HC12.print(",");
+    HC12.print(PS4.LStickX());
+    HC12.print(",");
+    HC12.print(PS4.LStickY());//if you just need to send 2 variables,simply change this value and the following to 0
+    HC12.print(",");
+    HC12.print(PS4.RStickX());//if you just need to send 3 variables,simply change this value and the next to 0
+    HC12.print(",");
+    HC12.print(PS4.RStickY());//if you just need to send 4 variables,simply change this value to 0
+    HC12.print(",");
+    HC12.print(PS4.R1());//if you just need to send 3 variables,simply change this value and the next to 0
+    HC12.print(",");
+    HC12.print(PS4.L1());//if you just need to send 4 variables,simply change this value to 0
+    HC12.println("");
+  //you can add even numbers of variables, ex add 2, 4, 6 ...(so 3, 5, 7, 9... variables in total) before this line by adding a variable line, then a comma line, another variable line then a second comma line under the 5th variable line but before the println.  
+  //for the purpose of simplicity, it is easier to add in groups of 2, so that less modification is necessary on the other end
+  //if you have more variables than you are using, change the variable to 0, as above explained, again for the purpose of simplicity
   }
-}  
 
-  delay(100);
 
 }
