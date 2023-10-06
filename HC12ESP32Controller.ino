31f000ac087badc297597813dc3e6bb7b7c5993e
StevonS <110857291+StevonS@users.noreply.github.com>
2023-10-06 16:56:35 -0400

    Update HC12ESP32Controller.ino

--- HC12ESP32Controller.ino
+++ HC12ESP32Controller.ino
@@ -2,15 +2,23 @@
 #define RXD2 16	//(RX2)
 #define TXD2 17	//(TX2)
 #define HC12 Serial2  //Hardware serial 2 on the ESP32
+
 bool armed = false;
 byte incomingByte;
-String readBuffer = "";
+String readBuffer;
 bool startup = false;
-int connectionStatus;
+int connectionStatus = 1;
 bool reply = false;
+bool connection = false;
+int LStickX;
+int LStickY;
+int RStickX;
+int RStickY;
+
+
 void setup() {
   Serial.begin(115200);
-  PS4.begin("EC:62:60:1E:E1:68");
+  PS4.begin("EC:62:60:1E:E1:6A");
   pinMode(5, OUTPUT);
   digitalWrite(5, LOW);           //Normally HIGH, LOW for settings
   Serial.begin(115200);           // Serial port to computer
@@ -20,36 +28,32 @@ void setup() {
 
 void loop() {
 
-  while(connection == false){
-    HC12.write("Connection Request");
-    while (HC12.available()) {
-      incomingByte = HC12.read();
-      readBuffer += char(incomingByte); 
-    }
-    if(readBuffer == "Connection Established"){
-      connection == true;
-      break;
-    }
-  }
+
+  
   
   controller();
+  if (connection == false){
+    HC12.write("Connection Request");
+  }
+  
+ 
   while (HC12.available()) {
     incomingByte = HC12.read();
     readBuffer += char(incomingByte);        
-    Serial.write(HC12.read());  
-
-  }
-  while (Serial.available()) {      
-    HC12.write(Serial.read());   
-
   }
 
   
+  if(readBuffer == "Connection Established" && connection == false){
+    connection = true;
+    Serial.println("Connection");
+  }
   
-  if (readBuffer == "On-Board Armed"){
+  Serial.println(readBuffer);
+  if (readBuffer == "Onboard Armed"){
     startup = true;
     Serial.println("Starting Transmission:");
   }
+  readBuffer = "";
   delay(200);
 }
 
@@ -58,6 +62,7 @@ void controller(){
 
   //Arm/Disarm
   if (PS4.PSButton()){
+    delay(500);
     armed = !armed;
     if (armed == true){
       Serial.println("!System Armed!");
@@ -67,23 +72,49 @@ void controller(){
       HC12.write("Disarmed");
     }
   }
+  
+  if (PS4.LStickX() > 15 || PS4.LStickX() < -15){
+    LStickX = PS4.LStickX();
+  }else{
+    LStickX = 0;
+  } 
+  if (PS4.LStickY() > 15 || PS4.LStickY() < -15){
+    LStickY = PS4.LStickY();
+  }else{
+    LStickY = 0;
+  } 
+  if (PS4.RStickY() > 15 || PS4.RStickY() < -15){
+    RStickX = PS4.RStickX();
+  }else{
+    RStickX = 0;
+  } 
+  if (PS4.RStickY() > 15 || PS4.RStickY() < -15){
+    RStickY = PS4.RStickY();
+  }else{
+    RStickY = 0;
+  } 
+
   //PS4.setLed(uint8_t r, uint8_t g, uint8_t b)
   if (armed == true && startup == true){
     HC12.print(PS4.R2Value());//sends the variables
     HC12.print(",");
-    HC12.print(PS4.LStickX());
+    HC12.print(LStickX);
     HC12.print(",");
-    HC12.print(PS4.LStickY());//if you just need to send 2 variables,simply change this value and the following to 0
+    HC12.print(LStickY);//if you just need to send 2 variables,simply change this value and the following to 0
     HC12.print(",");
-    HC12.print(PS4.RStickX());//if you just need to send 3 variables,simply change this value and the next to 0
+    HC12.print(RStickX);//if you just need to send 3 variables,simply change this value and the next to 0
     HC12.print(",");
-    HC12.print(PS4.RStickY());//if you just need to send 4 variables,simply change this value to 0
+    HC12.print(RStickY);//if you just need to send 4 variables,simply change this value to 0
     HC12.print(",");
     HC12.print(PS4.R1());//if you just need to send 3 variables,simply change this value and the next to 0
     HC12.print(",");
     HC12.print(PS4.L1());//if you just need to send 4 variables,simply change this value to 0
+    HC12.print(",");
+    HC12.print(connectionStatus);
+    HC12.print(",");
+    HC12.print("0");
     HC12.println("");
-    HC12.print(ConnectionStatus);
+    
   //you can add even numbers of variables, ex add 2, 4, 6 ...(so 3, 5, 7, 9... variables in total) before this line by adding a variable line, then a comma line, another variable line then a second comma line under the 5th variable line but before the println.  
   //for the purpose of simplicity, it is easier to add in groups of 2, so that less modification is necessary on the other end
   //if you have more variables than you are using, change the variable to 0, as above explained, again for the purpose of simplicity
